f955151463261ed5ad966134dd367f19ed54a137
Jovani Matthieu <73386424+jovani494@users.noreply.github.com>
2024-02-26 10:10:51 +0300

    Update user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,9 +3,10 @@ const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
     next();
   });
 
