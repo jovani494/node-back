1bf05776a9533fd95ae7398b54bd38bb0b6f1143
Jovani Matthieu <73386424+jovani494@users.noreply.github.com>
2024-03-01 14:40:16 +0300

    Update client.model.js

--- app/models/client.model.js
+++ app/models/client.model.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const ClientSchema = new mongoose.Schema({
   Nom: {
     type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
+    required: true
+    
   },
   Prenom: String,
   Gender : {
